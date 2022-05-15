39ea0a274c74b474683bc7228d78ff19e959a9be
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-05-15 19:45:25 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import {Users} from './users.js'
 
 function App() {
+  const[query, Setquery]=useState('');
+ 
+  const onchange=(e)=>
+  {
+   Setquery(e.target.value);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='search-bar'>
+      <input type='text' placeholder='search here' onChange={onchange}/>
+      <ul>
+ {
+   Users.filter((user)=> user.address.includes(query)).map((user)=>
+   (
+     <li key={user.id}>{user.address}</li>
+   ))
+ }
+       
+      </ul>
+
+    </div>
     </div>
   );
 }
---
+++ src/users.js
@@ -0,0 +1,55 @@
+
+    export const Users= [
+      {
+        "id": 0,
+        "name": "Adam Carter",
+        "work": "Unilogic",
+        "email": "adam.carter@unilogic.com",
+        "dob": "1978",
+        "address": "83 Warner Street",
+        "city": "Boston",
+        "optedin": true
+      },
+      {
+        "id": 1,
+        "name": "Leanne Brier",
+        "work": "Connic",
+        "email": "leanne.brier@connic.org",
+        "dob": "1987",
+        "address": "9 Coleman Avenue",
+        "city": "Toronto",
+        "optedin": false
+      },
+      {
+        "id": 2,
+        "name": "aakriti",
+        "work": "student",
+        "email": "leanne.brier@connic.org",
+        "dob": "1987",
+        "address": "tamghas gulmi",
+        "city": "Toronto",
+        "optedin": false
+      },
+      {
+        "id": 3,
+        "name": "sushmita chetri",
+        "work": "artist",
+        "email": "leanne.brier@connic.org",
+        "dob": "1987",
+        "address": "kalanki kathmandu",
+        "city": "Toronto",
+        "optedin": false
+      },
+
+      {
+        "id": 4,
+        "name": "nisha pela",
+        "work": "Connic",
+        "email": "leanne.brier@connic.org",
+        "dob": "1987",
+        "address": "mahendranagar",
+        "city": "Toronto",
+        "optedin": false
+      }
+    ]
+  
